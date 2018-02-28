0367c34c312a665469239dba22003797ec0b9c52
Steboss <stefanobosisio1@gmail.com>
2018-02-28 18:48:27 +0000

    Cleaning data and creation of dictionaries

---
+++ politics/berlusconi/dictionary/set_negative_reference/fix_neg.py
@@ -0,0 +1,22 @@
+import sys
+
+ofile = open("negative_reference.csv","w")
+pos = []
+with open(sys.argv[1],"r") as reader:
+    for line in reader:
+        pos.append(line.strip())
+
+pos.sort()
+
+#remove thedouble entities
+clean = []
+for entities in pos:
+    if entities in clean:
+        continue
+    elif "y" in entities:
+        continue #this is an English item
+    else:
+        clean.append(entities)
+
+for entities in clean:
+    ofile.write("%s\n" % (entities))         
