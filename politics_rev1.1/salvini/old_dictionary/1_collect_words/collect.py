9bd69f5cfcffc12211ccd1259cd6aee7d30742e7
Steboss <stefanobosisio1@gmail.com>
2018-03-05 15:42:27 +0000

    Tuned the dictionary, now try to use the comments are a training set

---
+++ politics_rev1.1/salvini/old_dictionary/1_collect_words/collect.py
@@ -0,0 +1,64 @@
+#script to collect all the important words from the total number of comment
+#run in python3
+import string
+
+# load the document
+filename = "3_clean.csv"
+stopwords = []
+with open("stopwords.csv","r") as reader:
+    for line in reader:
+        stopwords.append(line.strip())
+
+tokens_list = []
+#just for fun check which is the most used words
+dict_words = {}
+
+with open(filename,"r") as reader:
+    for line in reader:
+        tokens = line.split()
+        #remove punctuations
+        table_scheme = str.maketrans('','',string.punctuation)
+        tokens = [w.translate(table_scheme) for w in tokens]
+        #remove numbers?? for the momentskip it, since
+        #there could be something like 4marzo
+        #tokens= [word for word in tokens if word.isalpha()]
+        #now filter out the stopwords
+        tokens = [w for w in tokens if not w in stopwords]
+        #filter all the words by length
+        tokens = [word for word in tokens if len(word)>2]
+        for token in tokens:
+            if token in dict_words:
+                dict_words[token]+=1
+            else:
+                dict_words[token]=1
+            #remove the duplicates
+            if token in tokens_list:
+                continue
+            else:
+                tokens_list.append(token)
+
+print("Most recurring words...")
+#this is just to have fun
+max_val = 0
+values = []
+for keys in dict_words.keys():
+    values.append(dict_words[keys])
+
+
+values.sort()
+for i in range(len(values)-1,len(values)-20,-1):
+    for keys in dict_words.keys():
+        val = dict_words[keys]
+        if val == values[i]:
+            print(val, keys)
+
+#now save all the words in a filename
+#reduce the dictionary dimensions, the token
+#must have a value > 5
+
+print("Generation of a raw list of words...")
+ofile = open("raw_list_words.csv","w")
+for keys in dict_words.keys():
+    if dict_words[keys]>5:
+        ofile.write("%s\n" % keys)
+ofile.close()
--- politics_rev1.1/salvini/test/basic_dict/classifier.py
+++ politics_rev1.1/salvini/test/basic_dict/classifier.py
@@ -1,7 +1,8 @@
+
 from nltk.classify import NaiveBayesClassifier
 import sys
 import nltk
-
+import time
 def format_sentence(sent):
     return({word:True for word in  nltk.word_tokenize(sent)})
 
@@ -16,36 +17,59 @@ neg = []
 with open("basic_negative.csv","r") as reader:
     for line in reader:
         neg.append(line)
-neut = []
-with open("basic_neutral.csv","r") as reader:
-    for line in reader:
-        neut.append(line)
 
 
 positive_feature = [ (format_sentence(pos_term),"pos") for pos_term in pos]
 negative_feature = [ (format_sentence(neg_term),"neg") for neg_term in neg]
-neutral_feature  = [ (format_sentence(neut_term),"neut") for neut_term in neut]
 
-train_test = positive_feature + negative_feature  + neutral_feature
+train_test = positive_feature + negative_feature
 classifier = NaiveBayesClassifier.train(train_test)
 
 pos = 0
-neut = 0 
 neg = 0
-counter = 0 
+pos_line = 0
+neg_line = 0
+counter = 0
+count_line = 0
+#try to save the pos and neg comments
+#this can be helpful to perform a further machine learning test
+pos_file = open("positive_comments.csv","w")
+neg_file = open("negative_comments.csv","w")
+
 with open("dataset.csv","r") as reader:
     for line in reader:
+        count_line+=1 #this is the single comment
+        #print(line)
         words = line.split()
         for word in words:
+            pospos=0
+            negneg=0
+            #wordswords = []
             classRes = classifier.classify(format_sentence(word))
             if classRes=="neg":
                 neg +=1
+                negneg+=1
             if classRes=="pos":
                 pos +=1
-            if classRes =="neut":
-                neut+=1
-            counter+=1
+                pospos+=1
+        #check which are the positive comments
+        if pospos> negneg:
+            pos_line+=1
+            pos_file.write("%s\n" % line)
+        else:
+            neg_line+=1
+            neg_file.write("%s\n" % line)
 
-print("Pos: %.4f" % (float(pos)/float(counter)))
-print("Neg: %.4f" % (float(neg)/float(counter)))       
-print("Neut:%.4f" % (float(neut)/float(counter)))
+        #if count_line<50:
+        #    print(line)
+        #    print("neg:%d pos:%d" %(negneg,pospos))
+            #print(wordswords)
+        #    time.sleep(1)
+
+pos_file.close()
+neg_file.close()
+print("Pos raw %d out of %d" % (pos_line,count_line))
+print("Neg raw %d out of %d" % (neg_line,count_line))
+
+#print("Pos: %.4f" % (float(pos)/float(counter)))
+#print("Neg: %.4f" % (float(neg)/float(counter)))
