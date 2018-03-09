a9019b5a7088b593032725c6d7f29b12af03c0c4
Steboss <stefanobosisio1@gmail.com>
2018-03-09 11:50:49 +0000

    final analysis

--- politics_rev1.1/berlusconi/test/basic_machine_learning/classifier.py
+++ politics_rev1.1/berlusconi/test/basic_machine_learning/classifier.py
@@ -1,6 +1,7 @@
 
 from nltk.classify import NaiveBayesClassifier
 from nltk.classify.util import accuracy
+from nltk.metrics.scores import precision, recall
 import sys
 import nltk
 import time
@@ -10,14 +11,25 @@ def format_sentence(sent):
 
 #prepare the positive comments
 pos = []
+reference = {}
+reference["pos"]=[]
+reference["neg"]=[]
+testset={}
+testset["pos"]=[]
+testset["neg"]=[]
+
+#these are 100 values
 with open("basic_positive.csv","r") as reader:
     for line in reader:
         pos.append([format_sentence(line),"pos"])
+        reference["pos"].append(line)
+
 #negative comments
 neg = []
 with open("basic_negative.csv","r") as reader:
     for line in reader:
         neg.append([format_sentence(line),"neg"])
+        reference["neg"].append(line)
 
 #try to make a test  and training dataset
 #for the training take the 50 percent of the comments
@@ -25,51 +37,90 @@ with open("basic_negative.csv","r") as reader:
 #for the whole dataset take the whole pos and neg dataset and
 #create the test set with the all dataset file
 
-#Training part
+#Training part: last 50 elmeents
 training = pos[:int((.5)*len(pos))] + neg[:int((.5)*len(neg))]
 #this cryptic way to write it's just to select the last half part of the
 #data set (above)
-#and the first half dataset (below)
-test = pos[int((.5)*len(pos)):] + neg[int((.5)*len(neg)):]
-counter = 0
-test_dataset = []
-with open("basic_positive.csv","r") as reader:
-    for line in reader:
-        test_dataset.append(line)
-        counter+=1
-        if counter==25:
-            break
-
-print(len(test_dataset))
-counter= 0
-with open("basic_negative.csv","r") as reader:
-    for line in reader:
-        test_dataset.append(line)
-        counter+=1
-        if counter==25:
-            break
-print(len(test_dataset))
+#
+#test = pos[int((.5)*len(pos)):] + neg[int((.5)*len(neg)):]
 #this will be the testdataset
-
-
-#at the moment stick to the basic Bayes
+#at the moment stick to the basic Bayes and train it
 classifier = NaiveBayesClassifier.train(training)
 print("Most informative features...")
 print(classifier.show_most_informative_features())
 #now try with the test dataset and assess the accuracy
-print(accuracy(classifier, test))
-
-pos = 0
-neg = 0
-for line in test_dataset:
-
-    classRes = (classifier.classify(format_sentence(line)))
+for val in reference["pos"]:
+    classRes = classifier.classify(format_sentence(val))
     if classRes=="pos":
-        pos+=1
-    if classRes=="neg":
-        neg+=1
+        testset["pos"].append(val)
+    else:
+        testset["neg"].append(val)
 
-print("Total pos %d  and neg %d out of 50\n" % (pos,neg))
+for val in reference["neg"]:
+    classRes=classifier.classify(format_sentence(val))
+    if classRes=="pos":
+        testset["pos"].append(val)
+    else:
+        testset["neg"].append(val)
+
+true_pos = 0
+false_pos = 0 #negaitve in realitiy, but positive in prediction
+for val in testset["pos"]:
+    if val in reference["pos"]:
+        true_pos+=1
+    else:
+        false_pos+=1
+
+print("True pos: %d" %true_pos)
+true_neg = 0
+false_neg = 0 #positive in reality, but negative prediction
+
+for val in testset["neg"]:
+    if val in reference["neg"]:
+        true_neg+=1
+    else:
+        #the value is in reference["pos"]
+        #so this is a positive evaluated negative, so its a false negative
+        false_neg+=1
+print("True pos: %d, false_pos:%d, true neg: %d, false neg:%d"% (true_pos, false_pos, true_neg, false_neg))
+precision = true_pos/(true_pos+false_pos)
+recall = true_pos/(true_pos+false_neg)
+f1 = 2*((precision*recall)/(precision+recall))
+print("Precision: %.2f" % precision)
+print("Recall: %.2f" % recall)
+print("F1: %.2f" % f1)
+sys.exit(-1)
+#print(accuracy(classifier, test))
+
+#now try to measure  the accuracy, the precision and recall
+#create a dictionary to measure recall and precision
+
+#pos = 0
+#neg = 0
+#counter = 0
+#test_dataset = []
+#with open("basic_positive.csv","r") as reader:
+#    for line in reader:
+#        test_dataset.append(line)
+#        counter+=1
+#        if counter==25:
+#            break
+
+#counter= 0
+#with open("basic_negative.csv","r") as reader:#
+#    for line in reader:
+#        test_dataset.append(line)
+#        counter+=1
+#        if counter==25:
+#            break
+#for line in test_dataset:#
+#    classRes = (classifier.classify(format_sentence(line)))
+#    if classRes=="pos":
+#        pos+=1
+#    if classRes=="neg":
+#        neg+=1
+
+#print("Total pos %d  and neg %d out of 50\n" % (pos,neg))
 #now if the result is good enough, let's process the real dataset
 pos_line = 0
 neg_line = 0
